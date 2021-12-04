df4ad86add1142831b6092a885b2d4d405d4747b
scotthelberg <73599550+scotthelberg@users.noreply.github.com>
2021-12-04 10:39:38 -0600

    Update train_classifier.py

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,23 +1,93 @@
 import sys
+import nltk
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+import re
+import pandas as pd
+import pickle
+from sqlalchemy import create_engine
+from nltk.tokenize import word_tokenize
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
 
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import multioutput
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.metrics import classification_report
+from sklearn.model_selection import GridSearchCV
 
 def load_data(database_filepath):
-    pass
+    # load data from database
+    engine = create_engine('sqlite:///Disasters.db')
+    df = pd.read_sql_table('Disasters', engine)
+    X = df ['message'].values
+    y = df.iloc[:,4:]
+    return X, y, category_names
 
 
 def tokenize(text):
-    pass
+    #normalize text
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower())
+    
+    #split text into words using NLTK
+    words = word_tokenize (text)
+    
+    #remove stop words
+    words = [w for w in words if w not in stopwords.words("english")]
+    
+    #reduce words to their stems
+    stemmed = [PorterStemmer().stem(w) for w in words]
+    
+    #reduce words to their root form
+    lemmed_words = [WordNetLemmatizer().lemmatize(w) for w in words]
+    return lemmed_words
 
 
 def build_model():
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier (RandomForestClassifier()))
+    ])
+    
+    parameters = {
+    'clf__estimator__max_depth': [10, 20],
+    'clf__estimator__n_estimators': [75,200]
+        }
+
+    cv = GridSearchCV(pipeline, param_grid=parameters)
     pass
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
+    
+    X_train, X_test, y_train, y_test = train_test_split (X,y)
+
+    pipeline.fit(X_train, y_train)
+
+    y_pred = pipeline.predict(X_test)
+    
+    i =0
+    for col in y_test:
+        print('Feature {}:{}'.format(i+1,col))
+        print(classification_report(y_test[col],y_pred[:,i]))
+        i=i+1
+    accuracy = (y_pred == y_test.values).means()
+    print('The model accuracy score is {:.3f}'.format(accuracy))
+    
+    scores(y_test, y_pred)
     pass
 
 
 def save_model(model, model_filepath):
+    filename = 'disasters_model.sav'
+    pickle.dump(cv,open(filename,'wb'))
     pass
 
 
